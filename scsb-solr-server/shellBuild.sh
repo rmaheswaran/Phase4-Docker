f39df4fd378d27633697f29e30514e3de5d6a013
rajeshbabuk <rajeshbabu.k@htcindia.com>
2020-05-21 18:01:37 +0530

    Technology Upgrade: Solr 6.5.0 to Solr 8.5.1

--- scsb-solr-server/shellBuild.sh
+++ scsb-solr-server/shellBuild.sh
@@ -1,12 +1,12 @@
 # Add SCSB-SOLR files
-cd /opt/solr-6.5.0/bin
+cd /opt/solr-8.5.1/bin
 ./solr start -force
 ./solr create -c recap -force
-ls /opt/solr-6.5.0/server/solr/
-cp /opt/solr-config/solrconfig.xml /opt/solr-6.5.0/server/solr/recap/conf/solrconfig.xml
-cp /opt/solr-config/managed-schema /opt/solr-6.5.0/server/solr/recap/conf/managed-schema
-mkdir /opt/solr-6.5.0/server/solr/configsets/recap_config
+ls /opt/solr-8.5.1/server/solr/
+cp /opt/solr-config/solrconfig.xml /opt/solr-8.5.1/server/solr/recap/conf/solrconfig.xml
+cp /opt/solr-config/managed-schema /opt/solr-8.5.1/server/solr/recap/conf/managed-schema
+mkdir /opt/solr-8.5.1/server/solr/configsets/recap_config
 \
-mkdir /opt/solr-6.5.0/server/solr/configsets/recap_config/conf
+mkdir /opt/solr-8.5.1/server/solr/configsets/recap_config/conf
 \
-cp -R /opt/solr-6.5.0/server/solr/recap/conf /opt/solr-6.5.0/server/solr/configsets/recap_config/
+cp -R /opt/solr-8.5.1/server/solr/recap/conf /opt/solr-8.5.1/server/solr/configsets/recap_config/
