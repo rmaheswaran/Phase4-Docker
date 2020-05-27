2a7014a9fcef1907dbce1f9fbaaaed5bbb244946
mosesjoel <moses.joel@htcindia.com>
2020-05-27 17:13:28 +0530

    Update start.sh

--- scsb-mysql-8.0.20/start.sh
+++ scsb-mysql-8.0.20/start.sh
@@ -1,5 +1,5 @@
 sudo docker build -t phase4-scsb-mysql .
-sudo docker run --name phase4-scsb-mysql -e MYSQL_ROOT_PASSWORD=root -v /data/mysql-data-4:/var/lib/mysql -p 3306:3306 -d phase4-scsb-mysql
+sudo docker run --name phase4-scsb-mysql -e MYSQL_ROOT_PASSWORD=root -v /data/mysql-data-7:/var/lib/mysql -p 3306:3306 -d phase4-scsb-mysql
 sudo docker logs -f phase4-scsb-mysql
 sudo docker inspect phase4-scsb-mysql | grep IPAddress
 sudo docker exec -it phase4-scsb-mysql /bin/bash
