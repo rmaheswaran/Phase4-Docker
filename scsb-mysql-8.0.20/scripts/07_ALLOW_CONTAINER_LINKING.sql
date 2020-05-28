04f4150d3d606ca09b0c2af57f2d44a5c4392e36
mosesjoel <moses.joel@htcindia.com>
2020-05-28 14:36:14 +0530

    Update 07_ALLOW_CONTAINER_LINKING.sql

--- scsb-mysql-8.0.20/scripts/07_ALLOW_CONTAINER_LINKING.sql
+++ scsb-mysql-8.0.20/scripts/07_ALLOW_CONTAINER_LINKING.sql
@@ -4,3 +4,4 @@ GRANT ALL ON *.* TO 'recap'@'%' WITH GRANT OPTION;
 #GRANT ALL ON *.* to 'recap'@'%' IDENTIFIED BY 'recap';
 #GRANT ALL ON sonar.* TO 'sonar'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+ALTER USER 'recap'@'%' IDENTIFIED WITH mysql_native_password BY 'recap'; 
